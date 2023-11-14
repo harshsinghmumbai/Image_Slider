54b370ad07af3b1d113c17fe4a0d13c12edc6b87
Harsh Singh <harshrsingh552005@gmail.com>
2023-11-14 15:18:40 +0530

    image_slider
    all commit is here

---
+++ logic.js
@@ -0,0 +1,31 @@
+let flag = 0;
+
+function controller(x) {
+  flag = flag + x;
+  slideshow(flag);
+}
+
+function slideshow(num) {
+  let slide = document.getElementsByClassName("slide");
+  // console.log(slide);
+
+  if (num == slide.length) {
+    flag = 0;
+    num = 0;
+  }
+
+  if (num < 0) {
+    flag = slide.length - 1;
+    num = slide.length - 1;
+  }
+
+  for (let y of slide) {
+    y.style.display = "none";
+    // console.log(y);
+  }
+
+  slide[num].style.display = "block";
+  
+}
+
+slideshow(flag);
